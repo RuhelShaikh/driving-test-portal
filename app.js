3e7fa7fbb5886654081515dd1a8dd257c46c1d48
RuhelShaikh <ruhelshaikh786@gmail.com>
2024-09-07 17:40:26 +0530

    Implement MongoDB session store for production

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
 
@@ -36,7 +37,16 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      collectionName: "sessions",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: "strict", // Protect against CSRF
+    },
   })
 );
 
